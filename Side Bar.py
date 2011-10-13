24dcd410ba9c4ddc5f6b6a18b81835d29ab2c82a
Tito Bouzout <tito.bouzout@gmail.com>
2011-10-13 12:59:05 -0300

    add width and height attributes to images when copying as img tag.

--- Side Bar.py
+++ Side Bar.py
@@ -636,19 +636,88 @@ class SideBarFilesCopyTagAhrefCommand(sublime_plugin.WindowCommand):
 		return len(paths) > 0
 
 class SideBarFilesCopyTagImgCommand(sublime_plugin.WindowCommand):
+
 	def run(self, paths):
 		to_copy = []
 		for item in SideBarSelection(paths).getSelectedImages():
-			to_copy.append('<img src="'+item.pathAbsoluteFromProjectEncoded()+'" border="0"/>')
-		
+			try:
+				image_type, width, height = self.getImageInfo(item.contentBinary())
+				to_copy.append('<img src="'+item.pathAbsoluteFromProjectEncoded()+'" width="'+str(width)+'" height="'+str(height)+'" border="0"/>')
+			except:
+				to_copy.append('<img src="'+item.pathAbsoluteFromProjectEncoded()+'" border="0"/>')
+
 		sublime.set_clipboard("\n".join(to_copy));
 		if len(to_copy) > 1 :
 			sublime.status_message("Items copied")
 		else :
 			sublime.status_message("Item copied")
 
+	#Project:http://code.google.com/p/bfg-pages/
+	#License:http://www.opensource.org/licenses/bsd-license.php
+	def getImageInfo(self, data):
+		import StringIO
+		import struct
+		data = str(data)
+		size = len(data)
+		height = -1
+		width = -1
+		content_type = ''
+
+		# handle GIFs
+		if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
+			# Check to see if content_type is correct
+			content_type = 'image/gif'
+			w, h = struct.unpack("<HH", data[6:10])
+			width = int(w)
+			height = int(h)
+
+		# See PNG 2. Edition spec (http://www.w3.org/TR/PNG/)
+		# Bytes 0-7 are below, 4-byte chunk length, then 'IHDR'
+		# and finally the 4-byte width, height
+		elif ((size >= 24) and data.startswith('\211PNG\r\n\032\n')
+					and (data[12:16] == 'IHDR')):
+			content_type = 'image/png'
+			w, h = struct.unpack(">LL", data[16:24])
+			width = int(w)
+			height = int(h)
+
+		# Maybe this is for an older PNG version.
+		elif (size >= 16) and data.startswith('\211PNG\r\n\032\n'):
+			# Check to see if we have the right content type
+			content_type = 'image/png'
+			w, h = struct.unpack(">LL", data[8:16])
+			width = int(w)
+			height = int(h)
+
+		# handle JPEGs
+		elif (size >= 2) and data.startswith('\377\330'):
+			content_type = 'image/jpeg'
+			jpeg = StringIO.StringIO(data)
+			jpeg.read(2)
+			b = jpeg.read(1)
+			try:
+				while (b and ord(b) != 0xDA):
+					while (ord(b) != 0xFF): b = jpeg.read(1)
+					while (ord(b) == 0xFF): b = jpeg.read(1)
+					if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
+						jpeg.read(3)
+						h, w = struct.unpack(">HH", jpeg.read(4))
+						break
+					else:
+						jpeg.read(int(struct.unpack(">H", jpeg.read(2))[0])-2)
+					b = jpeg.read(1)
+				width = int(w)
+				height = int(h)
+			except struct.error:
+				pass
+			except ValueError:
+				pass
+		return content_type, width, height
+
 	def is_enabled(self, paths):
 		return SideBarSelection(paths).hasImages()
+
+	
 
 class SideBarFilesCopyTagStyleCommand(sublime_plugin.WindowCommand):
 	def run(self, paths):
