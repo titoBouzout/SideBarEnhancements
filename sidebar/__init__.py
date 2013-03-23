202380160f5c60b1244bceee373e6afd86107340
Klaas Cuvelier <klaas@showpad.com>
2013-03-23 12:18:21 +0100

    updates import

--- sidebar/__init__.py
+++ sidebar/__init__.py
@@ -1,4 +1,3 @@
-
-from . import SideBarItem as SideBarItem
-from . import SideBarSelection as SideBarSelection
-from . import SideBarProject as SideBarProject
+import SideBarItem as SideBarItem
+import SideBarSelection as SideBarSelection
+import SideBarProject as SideBarProject
