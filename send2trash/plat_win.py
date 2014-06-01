66f1655b3d1a544063a68bf346bf5cb3d9080733
Tito Bouzout <tito.bouzout@gmail.com>
2014-06-01 17:47:46 -0300

    Whitespace

--- send2trash/plat_win.py
+++ send2trash/plat_win.py
@@ -1,7 +1,7 @@
 # Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
 
-# This software is licensed under the "BSD" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+# This software is licensed under the "BSD" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.hardcoded.net/licenses/bsd_license
 
 from ctypes import windll, Structure, byref, c_uint
@@ -35,7 +35,7 @@ FOF_ALLOWUNDO = 64
 FOF_NOERRORUI = 1024
 
 def send2trash(path):
-		#	
+		#
     #if not isinstance(path, str):
     #    path = str(path, 'mbcs')
     #if not op.isabs(path):
