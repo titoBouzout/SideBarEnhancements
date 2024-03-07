b46fc1dd5d8d220d9c55877a1b959a3038e6b6ac
Tito Bouzout <tito.bouzout@gmail.com>
2024-03-07 04:42:40 -0300

    update

--- push.bat
+++ push.bat
@@ -1,5 +1,9 @@
 rem FF PTSD
 
-git tag 6.0.0
+git add --all
+git commit -m "update"
+
+git push --prune
+git tag 7.0.0
 git push origin --tags
-git push --delete origin 5.0.0
+git push --delete origin 6.0.0
